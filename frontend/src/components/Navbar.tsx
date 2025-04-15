03154e8ef6edb4db6b2bc0eadb611c7d56b65097
Conner Vigil <cjvigil@live.com>
2025-04-15 09:21:37 -0600

    add image to navbar for fun

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,7 +13,8 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl font-bold">
+        <div className="text-xl font-bold flex items-center">
+          <img src="/sponsorcx.png" alt="SponsorCX Logo" className="h-8 mr-2" />
           <Link to="/">Fullstack Challenge</Link>
         </div>
         <div className="flex space-x-6">
